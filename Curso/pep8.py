2529492d2817ab287cd791460dfaad0e2e2434c6
Roberto Lucas Maciel De Mendonça <72272899+lmaciel0@users.noreply.github.com>
2021-02-04 18:26:29 -0300

    Aula Finalizada.

--- Curso/pep8.py
+++ Curso/pep8.py
@@ -34,6 +34,63 @@ A ideia da PEP8 é que possamos escrever códigos Python de forma Pythônica.
 
 [4] - Linhas em Branco
 
+   - Separar funções e definições de classe com duas linhas em brano;
+   - Métodos dentro de uma classe devem ser separados com uma única linha em branco;
+
+[5] - Imports
+
+   - Imports devem ser sempre feitos em linhas separadas;
+   import sys
+   import os
+
+   # Não há problemas em utilizar :
+      from types import StringType, ListType
+
+# Caso tenha muitos imports de um mesmo pacote, recomenda-se fazer :
+   from types import (
+      StringType,
+      ListType
+      SetType
+      OutroType
+)
+
+# Imports devem ser colocados no topo do arquivo, logo depois de quaisquer comentarios ou docstrings e antes
+# de constantes ou variáveis globais.
+
+
+[6] - Espaços em expressões e instruções
+
+# Não faça :
+   funcao( algo[ 1 ], { outros : 2 } )
+
+# Faça:
+   funcao(algo[1], {outros: 2})
+
+# Não faça:
+   algo (1)
+
+# Faça
+   algo(1)
+
+# Não faça:
+   dict ['chave'] = lista [indice]
+
+# Faça:
+   disct['chave] = lista[indice]
+
+# Não faça:
+x              = 1
+y              = 3
+variavel longa = 5
+
+# Faça:
+   x = 1
+   y = 3
+   variavel_longa = 5
+
+
+[7] - Termine sempre uma instrução com uma nova linha
+
  """ 
 
-
+import this
