432d8b48018de8e58c5d089109578b83a0605700
Roberto Lucas Maciel De Mendonça <72272899+lmaciel0@users.noreply.github.com>
2021-02-04 12:58:33 -0300

    Alteração nas pastas e inicio do pep8

---
+++ Seção 3/pep8.py
@@ -0,0 +1,39 @@
+""" PEP8 - Python Enhacement Proposal 
+
+São propostas de melhorias para as linguagem Python
+
+Zem Of Python
+
+Import This
+
+A ideia da PEP8 é que possamos escrever códigos Python de forma Pythônica.
+
+[1] - Utilize Camel Case para nomes de classes;
+
+ class Calculadora:
+    pass
+
+ class calculadoraCientifica:
+    pass
+ 
+[2] - Utilize nomes em minúsculo, separados por underline para funções ou variáveis;
+    def soma():
+    pass
+
+    def soma_dois():
+    pass
+
+ numero = 4
+ 
+ numero_impar = 5
+
+[3] - Utilize 4 espaços para identação/1 
+
+ if'a' in 'banana':
+    print('tem')
+
+[4] - Linhas em Branco
+
+ """ 
+
+
